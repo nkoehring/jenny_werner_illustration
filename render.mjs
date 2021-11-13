ab40eec4ac8c2da5135d3a6af2c4bfdc3026f195
koehr <n@koehr.in>
2021-11-13 20:57:23 +0100

    fix gallery, render static pages

--- render.mjs
+++ render.mjs
@@ -1,63 +1,59 @@
-import { readFileSync, writeFile } from 'fs'
-import { dirname, join } from 'path'
+import { readFile, readdir, writeFile } from 'fs/promises'
+import { basename, dirname, join } from 'path'
 import * as Eta from 'eta'
+import Markdown from 'markdown-it'
 
 const __dirname = dirname(new URL(import.meta.url).pathname)
 const srcDir = join(__dirname, 'src')
 const targetDir = join(__dirname, 'dist')
+const galleriesPath = join(__dirname, 'galleries')
+const pagesPath = join(__dirname, 'pages')
+
+function settingsPath(fileName) {
+  return join(__dirname, 'settings', `${fileName}.json`)
+}
+
+async function readAllJSON(path, ext='.json') {
+  const contents = {}
+  const files = await readdir(path)
+
+  for (const fileName of files) {
+    if (fileName.endsWith(ext)) {
+      const absPath = join(path, fileName)
+      const key = fileName.slice(0, ext.length * -1)
+      contents[key] = JSON.parse(await readFile(absPath, 'utf-8'))
+    }
+  }
+
+  return contents
+}
+
+async function renderedPages() {
+  const pages = await readAllJSON(pagesPath)
+  const md = Markdown({
+    html: true,
+    breaks: true,
+    linkify: true,
+    typographer: true
+  })
+
+  Object.values(pages).forEach(p => p.body = md.render(p.body))
+  return pages
+}
 
 async function render() {
-  const cssVars = JSON.parse(readFileSync('./settings/styles.json', 'utf-8'))
-  const page = JSON.parse(readFileSync('./settings/page.json', 'utf-8'))
-  const menu = JSON.parse(readFileSync('./settings/menu.json', 'utf-8'))
-  const galleryOptions = JSON.parse(readFileSync('./settings/gallery.json', 'utf-8'))
-  const social = JSON.parse(readFileSync('./settings/footer.json', 'utf-8'))
+  const cssVars = JSON.parse(await readFile(settingsPath('styles'), 'utf-8'))
+  const page = JSON.parse(await readFile(settingsPath('page'), 'utf-8'))
+  const menu = JSON.parse(await readFile(settingsPath('menu'), 'utf-8'))
+  const galleryOptions = JSON.parse(await readFile(settingsPath('gallery'), 'utf-8'))
+  const social = JSON.parse(await readFile(settingsPath('footer'), 'utf-8'))
+  const gallery = await readAllJSON(galleriesPath)
+  const pages = await renderedPages()
 
   Eta.configure({ views: srcDir })
 
   const output = await Eta.renderFile('./index', {
-    cssVars, page, menu, galleryOptions, social,
-    gallery: {
-      // the path is generated like `/assets/img/$galleryName/$name.jpg`
-      'comic-art': [
-        { alt: 'foo1', name: 'img01' },
-        { alt: 'bar1', name: 'img02' },
-        { alt: 'baz1', name: 'img03' },
-        { alt: 'foo2', name: 'img04' },
-        { alt: 'bar2', name: 'img05' },
-        { alt: 'baz2', name: 'img06' },
-        { alt: 'foo3', name: 'img01' },
-        { alt: 'bar3', name: 'img02' },
-        { alt: 'baz3', name: 'img03' },
-        { alt: 'foo4', name: 'img04' },
-        { alt: 'bar4', name: 'img05' },
-        { alt: 'baz4', name: 'img06' },
-        { alt: 'foo5', name: 'img01' },
-        //{ alt: 'bar5', name: 'img02' },
-        //{ alt: 'baz5', name: 'img03' },
-        //{ alt: 'foo6', name: 'img04' },
-        //{ alt: 'bar6', name: 'img05' },
-        //{ alt: 'baz6', name: 'img06' },
-        //{ alt: 'foo7', name: 'img01' },
-        //{ alt: 'bar7', name: 'img02' },
-        //{ alt: 'baz7', name: 'img03' },
-        //{ alt: 'foo8', name: 'img04' },
-        //{ alt: 'bar8', name: 'img05' },
-        //{ alt: 'baz8', name: 'img06' }
-      ],
-      'portraits': [
-      ],
-      'painting': [
-      ],
-      'pixel-art': [
-      ],
-      'traditional': [
-      ],
-      'sewing': [
-      ],
-      'various': [
-      ],
-    }
+    cssVars, page, menu, galleryOptions, social, pages, gallery
   })
 
   const target = join(targetDir, 'index.html')
